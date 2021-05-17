6347195dfe5a35a20b5ab21358c7eb0083c99663
JuanC99 <juancarloshr123@gmail.com>
2021-05-17 16:38:44 +0200

    Implemetanción metodo calcular probabilidades

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -59,9 +59,9 @@ public class Deck : MonoBehaviour
 
     private void ShuffleCards()
     {
-        
 
-      
+
+       deckInGame.Clear();
        for(int i=0; i < faces.Length; i++)
         {
             deckInGame.Add(faces[i]);
@@ -122,20 +122,12 @@ public class Deck : MonoBehaviour
         else if (dealer.GetComponent<CardHand>().points == 21)
         {
             finalMessage.text = "Blackjack!! The dealer WIN!";
+           
             InteractButtons(false);
 
         }
     }
 
-    private void CalculateProbabilities()
-    {
-        /*TODO:
-         * Calcular las probabilidades de:
-         * - Teniendo la carta oculta, probabilidad de que el dealer tenga más puntuación que el jugador
-         * - Probabilidad de que el jugador obtenga entre un 17 y un 21 si pide una carta
-         * - Probabilidad de que el jugador obtenga más de 21 si pide una carta          
-         */
-    }
 
     void PushDealer()
     {
@@ -145,7 +137,7 @@ public class Deck : MonoBehaviour
         dealer.GetComponent<CardHand>().Push(deckInGame[cardIndex],GetNumberFromSprite(deckInGame[cardIndex]));
         cardIndex++;
         Debug.Log("puntos deler: "+ dealer.GetComponent<CardHand>().points);
-        
+        CalculateProbabilities();
     }
 
     void PushPlayer()
@@ -226,6 +218,8 @@ public class Deck : MonoBehaviour
         {
             finalMessage.text = "The player WIN!";
         }
+
+        dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
         InteractButtons(false);
 
     }
@@ -247,6 +241,114 @@ public class Deck : MonoBehaviour
     {
         hitButton.interactable = state;
         stickButton.interactable = state;
+        if(state == false)
+        {
+            dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
+        }
+      
     }
-    
+
+    private void CalculateProbabilities()
+    {
+        /*TODO:
+         * Calcular las probabilidades de:
+         * - Teniendo la carta oculta, probabilidad de que el dealer tenga más puntuación que el jugador
+         * - Probabilidad de que el jugador obtenga entre un 17 y un 21 si pide una carta
+         * - Probabilidad de que el jugador obtenga más de 21 si pide una carta          
+         */
+        if (dealer.GetComponent<CardHand>().cards.Count >= 1)
+        {
+
+            string textProb = "";
+
+            float probaDealerMayorPuntucaion=0.0f;
+        
+            int punuacionDealerSinCartaInicial = dealer.GetComponent<CardHand>().points - dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().value;
+
+            //numeros para superar la puntuacion del dealer
+            List<int> numerosSuperarDealer = NumerosParaSuperarValorConcreto(punuacionDealerSinCartaInicial, player.GetComponent<CardHand>().points);
+            for (int i = 0; i < numerosSuperarDealer.Count; i++)
+            {
+                //Prob de que la carta en juego sea uno de los numeros que supera el valor del jugador
+                probaDealerMayorPuntucaion += ProbSacarValor(numerosSuperarDealer[i]);
+            }
+            textProb += "Probabilidad de que el dealer tenga mas puntuacion: " + (probaDealerMayorPuntucaion * 100).ToString("0.00") + "%\n";
+
+            float probObtenerValorCercano21 = 0.0f;
+            List<int> NumeroEntre17y21 = NumerosParaSacaraValorEntre17y21(player.GetComponent<CardHand>().points);
+            for (int i = 0; i < NumeroEntre17y21.Count; i++)
+            {
+                probObtenerValorCercano21 += ProbSacarValor(NumeroEntre17y21[i]);
+            }
+            textProb += "Probabilidad sacar entre 17 y 21 con la siguiente carta: " + (probObtenerValorCercano21 * 100).ToString("0.00") + "%\n";
+
+
+            float probObtenerValorMayor21 = 0.0f;
+            List<int> numerosMayor21 = NumerosParaSuperarValorConcreto(player.GetComponent<CardHand>().points, 21);
+            for (int i = 0; i < numerosMayor21.Count; i++)
+            {
+                probObtenerValorMayor21 += ProbSacarValor(numerosMayor21[i]);
+            }
+            textProb += "Probabilidad sacar más de 21 con la siguiente carta: " + (probObtenerValorMayor21 * 100).ToString("0.00") + "%";
+
+            probMessage.text = textProb;
+        }
+        
+       
+       
+    }
+
+    private float ProbSacarValor(int valor)
+    {
+        int numeroCartasEnElMazo = (deckInGame.Count-cardIndex)+1;
+      
+        int contadorCarta =0;
+        List<Sprite> copyDeck= new List<Sprite>();
+        for (int i = cardIndex; i < deckInGame.Count; i++)
+        {
+            copyDeck.Add(deckInGame[i]);
+        }
+
+        //Añadimos a los calculos la carta del dealer porque no sabemos que carta es
+        copyDeck.Add(dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().front);
+
+        for (int i=0; i < copyDeck.Count; i++ )
+        {
+            if(GetNumberFromSprite(copyDeck[i]) == valor)
+            {
+              
+                contadorCarta++;
+            }
+        }
+        Debug.Log("card deck" + contadorCarta);
+        float res = (float)contadorCarta / (float)numeroCartasEnElMazo;
+        return res;
+    }
+
+    private List<int> NumerosParaSuperarValorConcreto(int valorInicial, int valorConcreto)
+    {
+        List<int> valores = new List<int>();
+        for (int i=1; i <= 10; i++)
+        {
+            if (valorInicial+i>valorConcreto)
+            {
+                valores.Add(i);
+            }
+        }
+        return valores;
+    }
+
+    private List<int> NumerosParaSacaraValorEntre17y21(int valorInicial)
+    {
+        List<int> valores = new List<int>();
+        for (int i = 1; i <= 10; i++)
+        {
+            if (valorInicial + i >=17 && valorInicial + i<=21)
+            {
+                valores.Add(i);
+            }
+        }
+        return valores;
+    }
+
 }
